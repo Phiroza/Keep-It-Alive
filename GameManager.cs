bf80c533fbfef2e3f18b128c17c8330227997263
Phiroza <61969527+Phiroza@users.noreply.github.com>
2020-04-20 01:46:34 +0200

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    //public static GameManager Instance;
+
+
+    /*void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }*/
+
+    public void Play()
+    {
+        
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Menu()
+    {
+        
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Intro()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+}
